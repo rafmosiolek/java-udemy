4e5ad1b3dc17d714508583d7d8ae9aa8dd689866
RafM <rkjmosiolek@gmail.com>
2018-08-20 10:47:39 +0100

    add char and boolean chapter and solved challenge

---
+++ CharAndBoolean/src/com/raf/Main.java
@@ -0,0 +1,19 @@
+package com.raf;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // chat has a width of 16 (2 bytes)
+	    char myChar = '%';
+        System.out.println(myChar);
+	    char copyrightSymbol = '\u00A9';
+        System.out.println(copyrightSymbol);
+
+        boolean myBool = true;
+        System.out.println(myBool);
+
+        char registeredSymbol = '\u00AE';
+        System.out.println(registeredSymbol);
+
+    }
+}
