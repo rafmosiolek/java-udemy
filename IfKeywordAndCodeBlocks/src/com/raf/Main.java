cf14580f82b1ec88484a04fc8f59290bfe0f8730
RafM <rkjmosiolek@gmail.com>
2018-08-23 09:40:01 +0100

    if else statements and codeblock exercises and challenge

---
+++ IfKeywordAndCodeBlocks/src/com/raf/Main.java
@@ -0,0 +1,20 @@
+package com.raf;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000) {
+            System.out.println("Your score is less than 5000!");
+        } else {
+            System.out.println("Got here!");
+        }
+
+
+    }
+}
