8bc4788a7a920e07ed370b76dbfd86b3f2e058ff
RafM <rkjmosiolek@gmail.com>
2018-08-23 10:33:39 +0100

    add displayHighScorePosition and calculateHighScorePosition methods

--- Methods/src/com/raf/Main.java
+++ Methods/src/com/raf/Main.java
@@ -4,8 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 1000, 800, 2000);
+        int playerScore = calculateScore(true, 1000, 800, 2000);
+        System.out.println("Your final score is " + playerScore + "!");
+
         calculateScore(false, 400, 200, 1000);
+
+        displayHighScorePosition("Player1", calculateHighScorePosition(1500));
+        displayHighScorePosition("Player2", calculateHighScorePosition(900));
+        displayHighScorePosition("Player3", calculateHighScorePosition(400));
+        displayHighScorePosition("Player4", calculateHighScorePosition(50));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -13,10 +20,29 @@ public class Main {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score is " + finalScore + "!");
             return finalScore;
         }
 
         return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int playerPositionTable) {
+
+        System.out.println(playerName + " managed to get into position " + playerPositionTable);
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        if (playerScore >= 1000) {
+            return 1;
+        }
+        else if (playerScore >= 500) {
+            return 2;
+        }
+        else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
