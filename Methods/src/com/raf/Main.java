5984a65b976927a8169c1c4fc7dfe2bf88a48c38
RafM <rkjmosiolek@gmail.com>
2018-08-23 10:17:21 +0100

    add exceptions for gameOver false; add return data type to the method

--- Methods/src/com/raf/Main.java
+++ Methods/src/com/raf/Main.java
@@ -5,15 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         calculateScore(true, 1000, 800, 2000);
-        calculateScore(true, 400, 200, 1000);
+        calculateScore(false, 400, 200, 1000);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("Your final score is " + finalScore + "!");
+            return finalScore;
         }
+
+        return -1;
     }
 }
