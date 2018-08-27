edea5c7617ef0d8ea72ddb225d3667de34237088
RafM <rkjmosiolek@gmail.com>
2018-08-27 10:27:30 +0100

    printDayOfTheWeek challenge

---
+++ printDayOfTheWeekChallenge/src/com/raf/Main.java
@@ -0,0 +1,48 @@
+package com.raf;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Write a method with the name printDayOfTheWeek that has one parameter of type int and name it day.
+        //  The method should not return any value (hint: void)
+        // Using a switch statement print Sunday, Monday, Saturday if the int parameter day is 0, 1, 6 respectively,
+        // otherwise it should print Invalid day.
+
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(-1);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(19);
+        printDayOfTheWeek(0);
+    }
+
+    private static void printDayOfTheWeek(int day) {
+
+        switch(day) {
+            case 1:
+                System.out.println("Monday!");
+                break;
+            case 2:
+                System.out.println("Tuesday!");
+                break;
+            case 3:
+                System.out.println("Wednesday!");
+                break;
+            case 4:
+                System.out.println("Thursday!");
+                break;
+            case 5:
+                System.out.println("Friday!");
+                break;
+            case 6:
+                System.out.println("Saturday!");
+                break;
+            case 7:
+                System.out.println("Sunday!");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+}
