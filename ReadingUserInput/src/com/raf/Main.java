1ba4075620226c0093da672adcae64e6243a51e5
RafM <rkjmosiolek@gmail.com>
2018-09-03 12:22:31 +0100

    handle the edge cases in readinguserinput

--- ReadingUserInput/src/com/raf/Main.java
+++ ReadingUserInput/src/com/raf/Main.java
@@ -8,17 +8,31 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
+        boolean hasNextInt = scanner.hasNextInt();
 
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        int age = currentYear - yearOfBirth;
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Your name is " + name);
-        System.out.println("Your age is " + age);
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            int age = currentYear - yearOfBirth;
+            String outputMessage;
+
+            if (age >= 0 && age <= 120) {
+                outputMessage = "Your name is " + name + ", and you are " + age + " years old.";
+            } else {
+                outputMessage = "ERROR: Invalid year of birth";
+            }
+
+            System.out.println(outputMessage);
+        } else {
+            System.out.println("Unable to parse a year of birth");
+        }
 
         scanner.close();
     }
