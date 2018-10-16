d26d7b0885b7eb297500b7aed08242f10a86a5a7
RafM <rkjmosiolek@gmail.com>
2018-10-16 14:13:39 +0100

    add player class and access public fields from a main class

---
+++ Encapsulation/src/raf/Main.java
@@ -0,0 +1,21 @@
+package raf;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Player player = new Player();
+        player.name = "Player1";
+        player.health = 10;
+        player.weapon = "knife";
+
+        int dmg = 5;
+        player.loseHealth(dmg);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
+        dmg = 7;
+        player.loseHealth(dmg);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
+    }
+}
